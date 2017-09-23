1c0a78d983e1ee8783b39c49029d0791348b7a9f
Yury Krivinets <yura_r37@bk.ru>
2017-09-23 14:44:52 +0300

    revert changes

--- Homework_2/src/Task4.java
+++ Homework_2/src/Task4.java
@@ -6,7 +6,8 @@ public class Task4 {
     public static String getCrypted(String text, String algo) throws NoSuchAlgorithmException {
         MessageDigest provider = MessageDigest.getInstance(algo);
         try {
-            return new String(provider.digest(text.getBytes(algo)), algo);
+            String encoding = "UTF-8";
+            return new String(provider.digest(text.getBytes(algo)), encoding);
         } catch (UnsupportedEncodingException e) {
             System.err.println(e.getMessage());
         }
